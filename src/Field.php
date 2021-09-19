f1a548a675c5783c8a7cb36270e62b6aa7fbb29b
Michal Tuček <michaltk1@gmail.com>
2021-09-19 18:58:00 +0200

    Base Field logic added

--- src/Field.php
+++ src/Field.php
@@ -4,5 +4,32 @@ namespace ShopUp\Acfoop;
 
 abstract class Field
 {
+	/** @var string */
+	private $key;
 
+	/** @var null|Field */
+	private $parent = null;
+
+	public function __construct(string $key)
+	{
+		$this->key = $key;
+	}
+
+	abstract public static function make(string $key): self;
+
+	/**
+	 * @return Field|null
+	 */
+	public function getParent(): ?Field
+	{
+		return $this->parent;
+	}
+
+	/**
+	 * @param Field|null $parent
+	 */
+	public function setParent(?Field $parent): void
+	{
+		$this->parent = $parent;
+	}
 }
