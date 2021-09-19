1ae1708d82df89f09cbd1afd5b91a58f0b14c360
Michal Tuček <michaltk1@gmail.com>
2021-09-19 18:56:10 +0200

    BlockBuilder added w/ class and id init

--- src/BlockBuilder.php
+++ src/BlockBuilder.php
@@ -4,5 +4,38 @@ namespace ShopUp\Acfoop;
 
 class BlockBuilder
 {
+	/**
+	 * @param array $_block
+	 * @return Block
+	 */
+	static function make(array $_block): Block
+	{
+		$block = new Block();
+		$block->setID(self::getBlockId($_block));
+		$block->setClasses(self::getBlockClasses($_block));
 
+		return $block;
+	}
+
+	/**
+	 * @param array $_block
+	 * @return string
+	 */
+	private static function getBlockId(array $_block): string
+	{
+		if (!empty($_block['anchor'])) {
+			return $_block['anchor'];
+		}
+
+		return 'block-' . $_block['id'];
+	}
+
+	/**
+	 * @param array $_block
+	 * @return array
+	 */
+	private static function getBlockClasses(array $_block): array
+	{
+		return array_merge($classes, explode(' ', $_block['className']));
+	}
 }
