f3fb8a64ab75c8ba2e846ba68293f793cd1a2124
Michal Tuček <michaltk1@gmail.com>
2021-09-20 18:42:13 +0200

    CloneField made abstract to be a base class for global blocks

--- src/Fields/Layout/CloneField.php
+++ src/Fields/Layout/CloneField.php
@@ -2,9 +2,27 @@
 
 namespace ShopUp\Acfoop\Fields\Layout;
 
+use Exception;
 use ShopUp\Acfoop\Fields\Field;
+use ShopUp\Acfoop\Traits\Renderable;
 
-class CloneField extends Field
+abstract class CloneField extends Field
 {
+	protected string $template;
 
+	use Renderable {
+		render as public renderContent();
+	}
+
+	/**
+	 * @return string
+	 * @throws Exception
+	 */
+	public function render(): string
+	{
+		if (!isset($this->template)) {
+			throw new Exception('You need to set the template in class ' . get_called_class());
+		}
+		return $this->renderContent($this->template);
+	}
 }
