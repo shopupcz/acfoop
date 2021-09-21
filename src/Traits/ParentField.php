d5cb9c2eede7b12e019f0e1ce50e6fed8b524be5
Michal Tuček <michaltk1@gmail.com>
2021-09-21 13:35:54 +0200

    Minor fix for children duplication when cloning the ParentField

--- src/Traits/ParentField.php
+++ src/Traits/ParentField.php
@@ -45,6 +45,23 @@ trait ParentField
 	}
 
 	/**
+	 * FIXME: Isn't there a better way to to this?
+	 *  Try to Google how to work with references in foreach loop.
+	 *
+	 * @return static
+	 */
+	public function duplicate(): Field
+	{
+		$clone = clone($this);
+		$children = [];
+		foreach ($clone->children as $child) {
+			$children[] = $child->duplicate();
+		}
+		$clone->setChildren($children);
+		return $clone;
+	}
+
+	/**
 	 * @param string $child
 	 *
 	 * @return Field
